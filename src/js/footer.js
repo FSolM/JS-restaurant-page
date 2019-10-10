34bd05aae649d9d87781820da5075f9c6150a72f
Carlos Sol <fersolmarin@hotmail.com>
2019-10-10 10:58:18 -0500

    Footer Injection Added

---
+++ src/js/footer.js
@@ -0,0 +1,7 @@
+const injectFooter = (target) => {
+  target.innerHTML = `
+  <div class="col-12">Powered by <a href="https://github.com/FSolM/Tejuino">Tejuino</a></div>
+  `;
+};
+
+export default injectFooter;
