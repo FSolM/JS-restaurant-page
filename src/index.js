3e389b217fd3e897f93ef8084d761b692b93858f
Carlos Sol <fersolmarin@hotmail.com>
2019-10-11 12:08:06 -0500

    clear function Added

--- src/index.js
+++ src/index.js
@@ -8,24 +8,31 @@ const navbar = document.getElementById('navbar');
 const content = document.getElementById('container-content');
 const footer = document.getElementById('footer');
 
+const clear = (target) => {
+  target.innerHTML = '';
+};
+
 injectFooter(footer);
 injectNavbar(navbar);
 renderHome(content);
 
 document.getElementById('tab-home').addEventListener('click', () => {
   if (document.title !== 'Home') {
+    clear(content);
     renderHome(content);
   }
 });
 
 document.getElementById('tab-menu').addEventListener('click', () => {
   if (document.title !== 'Menu') {
+    clear(content);
     renderMenu(content);
   }
 });
 
 document.getElementById('tab-contact').addEventListener('click', () => {
   if (document.title !== 'Contact') {
+    clear(content);
     renderContact(content);
   }
 });
